6c059108525e4f4de7b12edbdb8716d340dd8124
ronildoferreira <ronildo.ufpi@gmail.com>
2019-09-29 16:56:34 -0300

    Initial commit

---
+++ offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1569785982,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"images/sprite-sheet0.png",
+		"images/imagem-sheet0.png",
+		"images/canvas.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
